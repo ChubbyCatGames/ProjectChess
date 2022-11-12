4b1005e3b39fd28333b6e623c67ce9cb3f62b844
RelSouls <rel.miguel77@gmail.com>
2022-11-12 20:31:58 +0100

    Music for menu and game
    Music for menu implemented (in loop), preserved after changing scene. Music for the game is just set in loop.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    [SerializeField] GameObject music;
+
+    private void Awake()
+    {
+        //-----------Play Music control----------
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+
+        if (objs.Length > 1)
+        {
+            Destroy(objs[1].gameObject);
+        }
+
+        if (objs.Length != 0) music = objs[0];
+
+        if (music != null)
+        {
+            DontDestroyOnLoad(music);
+            if (!music.GetComponent<AudioSource>().isPlaying) music.GetComponent<AudioSource>().Play();
+        }
+        //---------------------------------------------
+    }
+
     public void GameScene(string nameScene)
     {
+        //------------Music things-------------
+        if (nameScene == "FirstTry") //Change this value according to the name of the Ingame scene
+            if(music != null)
+                music.GetComponent<AudioSource>().Stop();
+        //--------------------------------------
+
         SceneManager.LoadScene(nameScene);
     }
 
