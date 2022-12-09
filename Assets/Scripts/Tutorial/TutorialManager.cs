0495894f2e7e6983d59f796c5f6fc2fbe0001c74
SouthBarbol <73181761+SouthBarbol@users.noreply.github.com>
2022-12-09 22:03:04 +0100

    Tutorial

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -24,6 +24,14 @@ public class TutorialManager : MonoBehaviour
     {
         for (int i = 0; i < popUps.Length; i++)
         {
+            //popUp[0]  Mensaje de bienvenida y dice al jugador que mueva una pieza
+            //popUp[1]  Bien hecho y aventurate a probar mas casillas sorpresas, quizás tengas suerte
+            //popUp[2]  Explicacion de porque han subido los recursos (economia) y subir pieza a caballo o alfil
+            //popUp[3]  Foto de las posibles ascensiones
+            //popUp[4]  Mensaje de entabla pelea
+            //popUp[5]  Decirte buena pelea y explicar las peleas con foto
+            //popUp[6]  Ganar el combate y aparezca la exlicacion de que hay que matar al rey
+            //Tras el popUp 5 comerse al rey
             if (i== popUpIdx)
             {
                // popUps[i].SetActive(true);
@@ -35,11 +43,80 @@ public class TutorialManager : MonoBehaviour
         }
         //Debug.Log(board.getSelectedPiece().occupiedSquare);
             //mueva una pieza
-            if (board.GetPieceOnSquare(new Vector2Int(4,2)) != null){
-                Debug.Log("hola");
+
+       
+
+        if (board.GetPieceOnSquare(new Vector2Int(4,2)) != null || board.GetPieceOnSquare(new Vector2Int(5, 2)) != null || board.GetPieceOnSquare(new Vector2Int(4, 1)) != null || board.GetPieceOnSquare(new Vector2Int(5, 1)) != null || board.GetPieceOnSquare(new Vector2Int(5, 3)) != null || board.GetPieceOnSquare(new Vector2Int(4, 3)) != null)
+        {
+          popUpIdx ++;  //HAY QUE LIMITAR QUE EL JUGADOR SOLO PUEDA MOVEZ LA PIEZA DE LA SEGUNDA COLUMNA
+        }
+
+        else if (popUpIdx == 1)
+        {
+            if (/*si esta ready pa promote*/)
+            {
+                popUpIdx++; 
             }
+
+        }
+   
+
+        else if (popUpIdx == 2)
+        {
+            if (/*ya ha ascendido*/)
+            {
+                popUpIdx++;
+            }
+
+        }
+
+        else if (popUpIdx == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                popUpIdx++;
+            }
+
+        }
+
+        //POPUP de vamos pelea ya ome ya
+
+        else if (popUpIdx == 4)
+        {
+            if (piece.life < piece.maxLife)
+            {
+                popUpIdx++;
+            }
+
+        }
+
         
-        
+
+        //pop up de ole buena pelea er tio ahi
+
+        else if (popUpIdx == 5)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                popUpIdx++;
+            }
+
+        }
+
+        //fotito de  objetivo de la aprtida
+
+        else if (popUpIdx == 6)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                popUpIdx++;
+            }
+
+        }
+
+        //Despues de esta pantalla generar pieza y que se coma al rey
+
+
     }
 
 
