2855806b8383b05a4409d1671ed5c7caee8403dd
david-3lm <d.3lopm@gmail.com>
2022-12-11 19:50:52 +0100

    Last fixes

--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Video;
 
 public class VideoController : MonoBehaviour
 {
     [SerializeField] UnityEvent fadeBlackEvent;
     [SerializeField] UnityEvent endVideoEvent;
-
+    private VideoPlayer video;
 
     private void Awake()
     {
         StartCoroutine(FadeBlack());
         StartCoroutine(EndVideo());
+        video = GetComponent<VideoPlayer>();
+    }
+    private void Start()
+    {
+        video.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Teaser Beta Chess Holy War.mp4");
+        video.Play();
     }
 
     IEnumerator FadeBlack()
